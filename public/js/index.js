4b3df96c78ee8108feb9ac6193b5fea604e543e4
vinodf2f <vinodyevatikar95@gmail.com>
2019-01-03 21:21:14 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+  let messages = $('#messages');
+  let newMessage = messages.children('li:last-child')
+
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +33,7 @@ let html = Mustache.render(template, {
   createdAt: formattedTime
 });
 $('#messages').append(html);
+scrollToBottom();
 
 });
 
@@ -30,6 +46,8 @@ let html = Mustache.render(template, {
   createdAt: formattedTime
 });
 $('#messages').append(html);
+scrollToBottom();
+
 });
 
 $('#message-form').on('submit', function (e) {
