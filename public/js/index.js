23087ec805e7440f8f7b4d1399adc3e5cac255b9
vinodf2f <vinodyevatikar95@gmail.com>
2019-01-03 20:38:53 +0530

    Add mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -9,23 +9,28 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  let formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime} : ${message.text}`);
 
-  $('#messages').append(li);
+let formattedTime = moment(message.createdAt).format('h:mm a');
+let template = jQuery('#message-template').html();
+let html = Mustache.render(template, {
+  text: message.text,
+  from: message.from,
+  createdAt: formattedTime
+});
+$('#messages').append(html);
+
 });
 
 socket.on('newLocationMessage', function (message)  {
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = $('<li></li>');
-  let a = $('<a target="_blank">My current Location</a>');
-
-  li.text(`${message.from}:${formattedTime} `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
-})
+let template = jQuery('#location-message-template').html();
+let html = Mustache.render(template, {
+  url: message.url,
+  from: message.from,
+  createdAt: formattedTime
+});
+$('#messages').append(html);
+});
 
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
