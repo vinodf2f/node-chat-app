6b0014eff18b633003ec177d5ed8932b4dd994b3
vinodf2f <vinodyevatikar95@gmail.com>
2019-01-03 10:01:05 +0530

    to find diff not sending location

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,17 @@ socket.on('newMessage', function (message) {
   $('#messages').append(li);
 });
 
-$('#message-form').on('submit', function (e) {
+socket.on('newLocationMessage', function (message)  {
+  let li = jQuery('<li></li>');
+  let a = jQuery('<a target="_blank">My current Location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+})
+
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
   socket.emit('createMessage', {
@@ -25,4 +35,22 @@ $('#message-form').on('submit', function (e) {
   }, function () {
 
   });
+});
+
+
+let locationButton = $('#send-location');
+
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return  alert('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage ', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }, () => {
+    alert('Unable to fetch location');
+  })
 });
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
@@ -27,16 +27,15 @@ const port = process.env.PORT || 3000;
       console.log('createMessage', message);
       io.emit('newMessage', generateMessage(message.from, message.text));
       callback('This is from the server.');
-      // socket.broadcast.emit('newMessage', {
-      //   from: message.from,
-      //   text: message.text,
-      //   createdAt: new Date().getTime()
-      // });
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
         console.log('user was disconnected');
-    })
+    });
 });
  server.listen(port, () => {
      console.log(`Server is up on port ${port}`)});
